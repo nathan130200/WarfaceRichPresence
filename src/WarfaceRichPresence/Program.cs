af9abcd3efa287f9c17bd9bca84cb5df003c5002
FRNathan13 <nathan130200@gmail.com>
2018-09-16 10:38:49 -0300

    Adicionado código básico.

--- src/WarfaceRichPresence/Program.cs
+++ src/WarfaceRichPresence/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -8,11 +9,20 @@ namespace WarfaceRichPresence
 {
 	static class Program
 	{
+		static Mutex mutex = new Mutex(false, "Global\\Warface Rich Presence");
+
 		[STAThread]
 		static void Main()
 		{
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(true);
+
+			if (Utilities.ShouldWaitOne(ref mutex))
+				Application.Run(new Forms.MainForm());
+			else
+			{
+				MessageBox.Show("Já existe uma instância em execução!", null, MessageBoxButtons.OK, MessageBoxIcon.Error);
+			}
 		}
 	}
 }
---
+++ src/WarfaceRichPresence/Utilities.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+
+namespace WarfaceRichPresence
+{
+	public static class Utilities
+	{
+		public static bool ShouldWaitOne(ref Mutex mutex)
+		{
+#if DEBUG
+			return false;
+#else
+			return mutex.WaitOne();
+#endif
+		}
+	}
+}
